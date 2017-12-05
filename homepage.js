fe01cc9157dcfdee5cfa57ea86d048331f40e6c8
Jamie Olson <olsonjamiet@gmail.com>
2017-12-04 21:28:57 -0800

    API search feature set up

--- homepage.js
+++ homepage.js
@@ -1,3 +1,4 @@
+$(document).ready(function() {
 // Event listener for search button
 $("#run-search").on("click", function(event) {
 
@@ -5,20 +6,24 @@ event.preventDefault();
 
 // Create a variable for the user input
 var search = $("#user-input").val().trim();
-var search = $(this).attr("data-search");
 
 // Construct a URL to search user input in API
+
 var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" +
-        search + "&lat=34.0353061&lon=-118.3794192&radius=13%2C000";
+        search + "&lat=34.0353061&lon=-118.3794192&radius=13%2C000" + "&apikey=abf456dfeacd356e5b295aa8cb32a4a6";
 
 // AJAX Request
 $.ajax({
           url: queryURL,
           method: "GET"
         })
-        .done(function(response) {
 
 // Store the search results in a variable
         .done(function(response) {
           // Storing an array of results in the results variable
-          var results = response.data;
+          var results = response.restaurants;
+
+          console.log(results);
+ });
+         });
+});
