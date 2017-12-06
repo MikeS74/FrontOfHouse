fd85da6acbed1c2372759ac5b12d8a561b87ffc0
Michael Sanchez <futurefreq@hotmail.com>
2017-12-06 14:11:53 -0800

    grab zomato id working

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,47 +1,52 @@
-$(document).ready(function() {
-// Event listener for search button
-$("#run-search").on("click", function(event) {
+$(document).ready(function () {
 
-event.preventDefault();
+    // Event listener for search button
+    $("#run-search").on("click", function (event) {
 
-// Create a variable for the user input
-var search = $("#user-input").val().trim();
-    
-    $("#search-bar-div").hide();
+        event.preventDefault();
 
-// Construct a URL to search user input in API
+        // Create a variable for the user input
+        var search = $("#user-input").val().trim();
 
-var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" +
-        search + "&lat=34.0353061&lon=-118.3794192&radius=13%2C000" + "&apikey=abf456dfeacd356e5b295aa8cb32a4a6";
+        $("#search-bar-div").hide();
 
-// AJAX Request
-$.ajax({
-          url: queryURL,
-          method: "GET"
-        })
+        // Construct a URL to search user input in API
+        var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" +
+            search + "&lat=34.0353061&lon=-118.3794192&radius=13%2C000" + "&apikey=abf456dfeacd356e5b295aa8cb32a4a6";
 
-// Store the search results in a variable
-        .done(function(response) {
-          // Storing an array of results in the results variable
-          var results = response.restaurants;
+        // AJAX Request
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            // Store the search results in a variable
+            .done(function (response) {
 
-          console.log(results);
-    
-for (var i = 0; i < results.length; i++) {
+                // Storing an array of results in the results variable
+                var results = response.restaurants;
 
-                var listDiv = $("<div class='list-gen'>");
-                var restResults = response.restaurants[i];
-                var restName = restResults.restaurant.name;
-                var restLoc = restResults.restaurant.location.address;
-                var restRating = restResults.restaurant.user_rating.aggregate_rating;
-                var dispAll = $("<p class='rest-list-item'>").html(restName + "<br>" + restLoc + "<br>" + "Rating: " + restRating + "<br>");
-                listDiv.append(dispAll);
-    console.log(restLoc);
- 
-    $("#list-view").append(listDiv);
-};  
-    
-    
-});
-});
+                console.log(results);
+
+                for (var i = 0; i < results.length; i++) {
+
+                    var listDiv = $("<div class='list-gen'>");
+                    var restResults = response.restaurants[i];
+                    var restName = restResults.restaurant.name;
+                    var restLoc = restResults.restaurant.location.address;
+                    var restRating = restResults.restaurant.user_rating.aggregate_rating;
+                    var restID = restResults.restaurant.id;
+                    var dispAll = $("<p class='rest-list-item'>").html(restName + "<br>" + restLoc + "<br>" + "Rating: " + restRating + "<br>").attr({
+                        "data-zomato-id": restID
+                    });
+                    listDiv.append(dispAll);
+                    console.log(restLoc + " " + restID);
+
+                    $("#list-view").append(listDiv);
+                };
+                $(".rest-list-item").on("click", function (event) {
+                    var uniqueRestID = $(this).attr("data-zomato-id");
+                    console.log("Grab Zomato ID On Click Test " + uniqueRestID)
+                });
+            });
+    });
 });
