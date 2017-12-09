5b5bbec275a4fbf6bf92208f9a1aa7d4c9ad7d27
Michael Peltzer <mwpeltzer@gmail.com>
2017-12-09 11:00:14 -0800

    added more database shit

--- schema.sql
+++ schema.sql
@@ -8,7 +8,14 @@ CREATE TABLE reviews (
   api_id INT NOT NULL AUTO_INCREMENT,
   user_id VARCHAR(45) NULL,
   user_name VARCHAR(45) NULL,
-  review VARCHAR(300) NULL,
+  business_name VARCHAR(45) NOT NULL,
+  business_review VARCHAR(200) NOT NULL,
+  star_rating INT NOT NULL,
+  overall_rating INT NOT NULL,
+  pros VARCHAR(200) NULL,
+  cons VARCHAR(200) NULL,
+  suggestion_to_mgmt VARCHAR (200) NOT NULL,
+  date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,  
   PRIMARY KEY (api_id)
 );
 
