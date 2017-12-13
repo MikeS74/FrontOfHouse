a71d9680a334170a9df8a625912d7a4d7f96b78e
Michael Sanchez <futurefreq@hotmail.com>
2017-12-13 02:25:11 -0800

    added form styling

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,6 +6,10 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
     
+    app.get("/form", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/review.html"));
+    });
+    
     // app.get("*", function(req, res) {
     //     res.sendFile(path.join(__dirname, "../public/home.html"));
     // });
